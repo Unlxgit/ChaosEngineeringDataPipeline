da16169fbb3ba53126639a8300530f6da39d2379
Max Bürkner <max.buerkner@student.fh-kiel.de>
2024-05-20 18:54:21 +0200

    summerising deployment in one script

--- buildanddeploy.ps1
+++ buildanddeploy.ps1
@@ -3,9 +3,10 @@ kubectl delete service --all
 
 docker build -t forecast:latest ./src/forecast/
 docker build -t api-gateway:latest ./src/gateway/
+docker build -t gas-price-pull:latest ./src/gas_price_pull/
+
 
 kubectl apply -f ./deploy/deployment.yaml
 
 minikube service api-gateway-service -n gasai --url
 
-kubectl apply -f ./deploy/deployment_postgres.yaml
